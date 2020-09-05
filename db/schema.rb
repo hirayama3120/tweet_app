0d4194d70373f4aa798a81ee5bb16512df1e6854
Your Name <hirayama312@gmail.com>
2020-09-05 23:33:12 +0900

    投稿とユーザーを紐づけ

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_29_074606) do
+ActiveRecord::Schema.define(version: 2020_09_05_143022) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
